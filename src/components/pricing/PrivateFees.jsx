0477eb3315d29c24745a7c69ce1132f7407e29e4
zisam shaikh <zisamshaikh7@gmail.com>
2023-12-10 03:42:35 +0600

    commit

--- src/components/pricing/PrivateFees.jsx
+++ src/components/pricing/PrivateFees.jsx
@@ -28,14 +28,62 @@ const PrivateFees = () => {
     <>
       <section className="w-full bg-white">
         <div className="py-24 mx-auto container px-10 ">
-        <h2 className="text-center text-5xl my-2 capitalize font-semibold text-primary">
-        Parish Dental Pricing
-      </h2>
-      <h2 className="text-center text-xl my-2 capitalize text-primary">
-      Special Announcement from Parish Dental <br /> At Parish Dental, we take immense pride in being recognized as the market leader in dental pricing. Our dedication to offering top-notch dental services at the most competitive rates is unwavering. <br />Our Price Match Promise <br />We're committed to providing not only the best dental care but also the best value. Therefore, we guarantee to beat the price of any dental treatment offered by our competitors. Just present us with a written quote from another provider, and we will offer you a better price.<br/>Choose Parish Dental for:<br/>Affordable Excellence: We ensure that superior dental care is accessible to everyone.<br/>Comprehensive Care: From routine check-ups to advanced treatments, we offer a wide range of dental services.<br/>Trusted Expertise: Our team of skilled dental professionals is dedicated to your oral health.<br/>
-      </h2>
+          <h2 className="text-center text-5xl my-2 capitalize font-semibold text-primary">
+            Parish Dental Pricing
+          </h2>
+          <h2 className="text-center text-xl my-2 capitalize text-primary">
+            Special Announcement from Parish Dental <br /> At Parish Dental, we
+            take immense pride in being recognized as the market leader in
+            dental pricing. Our dedication to offering top-notch dental services
+            at the most competitive rates is unwavering. <br />
+            Our Price Match Promise <br />
+            We're committed to providing not only the best dental care but also
+            the best value. Therefore, we guarantee to beat the price of any
+            dental treatment offered by our competitors. Just present us with a
+            written quote from another provider, and we will offer you a better
+            price.
+            <br />
+            Choose Parish Dental for:
+            <br />
+            Affordable Excellence: We ensure that superior dental care is
+            accessible to everyone.
+            <br />
+            Comprehensive Care: From routine check-ups to advanced treatments,
+            we offer a wide range of dental services.
+            <br />
+            Trusted Expertise: Our team of skilled dental professionals is
+            dedicated to your oral health.
+            <br />
+          </h2>
 
           <div className="mt-3">
+            <div>
+              <h3 className="text-primary font-semibold text-2xl mt-8 mb-2">
+                Periodontal / Gum Treatment{" "}
+              </h3>
+
+              <div
+                className="flex justify-between items-center"
+              >
+                <div className="flex justify-between items-center w-full border-b">
+                  <h5 className="text-base lg:text-xl text-gray-600 my-4 w-3/5 max-w-[600px] ">
+                  Scale & Polish                    </h5>
+                  <p className="text-base lg:text-lg text-secondary font-medium w-2/5">
+                    £50
+                  </p>
+                </div>
+                <div
+                className="flex justify-between items-center"
+              >
+                <div className="flex justify-between items-center w-full border-b">
+                  <h5 className="text-base lg:text-xl text-gray-600 my-4 w-3/5 max-w-[600px] ">
+                  Scale & Polish                    </h5>
+                  <p className="text-base lg:text-lg text-secondary font-medium w-2/5">
+                    £50
+                  </p>
+                </div>
+              </div>
+            </div>
             <PrivateFeeFiltered category={null} />
             <PrivateFeeFiltered category="White Filling" />
             <PrivateFeeFiltered category="Root Canal" />
@@ -51,145 +99,147 @@ const PrivateFees = () => {
                 Aesthetics
               </h3>
               <div className="pla">
-              <details>
-                <summary>
-                  <h3 className="text-primary font-semibold text-xl	 mt-8 mb-2">
-                    Dermal Fillers
-                  </h3>
-                </summary>
-                <div className="flex justify-between items-center">
-                  <div className="flex justify-between items-center w-full border-b">
-                    <h5 className="text-base lg:text-xl text-gray-600 my-4 w-3/5 max-w-[600px] ">
-                      0.5ML
-                    </h5>
-                    <p className="text-base lg:text-lg text-secondary font-medium w-2/5">
-                      £100
-                    </p>
+                <details>
+                  <summary>
+                    <h3 className="text-primary font-semibold text-xl	 mt-8 mb-2">
+                      Dermal Fillers
+                    </h3>
+                  </summary>
+                  <div className="flex justify-between items-center">
+                    <div className="flex justify-between items-center w-full border-b">
+                      <h5 className="text-base lg:text-xl text-gray-600 my-4 w-3/5 max-w-[600px] ">
+                        0.5ML
+                      </h5>
+                      <p className="text-base lg:text-lg text-secondary font-medium w-2/5">
+                        £100
+                      </p>
+                    </div>
                   </div>
-                </div>
-                <div className="flex justify-between items-center">
-                  <div className="flex justify-between items-center w-full border-b">
-                    <h5 className="text-base lg:text-xl text-gray-600 my-4 w-3/5 max-w-[600px] ">
-                      0.7ML
-                    </h5>
-                    <p className="text-base lg:text-lg text-secondary font-medium w-2/5">
-                      £150
-                    </p>
+                  <div className="flex justify-between items-center">
+                    <div className="flex justify-between items-center w-full border-b">
+                      <h5 className="text-base lg:text-xl text-gray-600 my-4 w-3/5 max-w-[600px] ">
+                        0.7ML
+                      </h5>
+                      <p className="text-base lg:text-lg text-secondary font-medium w-2/5">
+                        £150
+                      </p>
+                    </div>
                   </div>
-                </div>
-                <div className="flex justify-between items-center">
-                  <div className="flex justify-between items-center w-full border-b">
-                    <h5 className="text-base lg:text-xl text-gray-600 my-4 w-3/5 max-w-[600px] ">
-                      1ML
-                    </h5>
-                    <p className="text-base lg:text-lg text-secondary font-medium w-2/5">
-                      £250
-                    </p>
+                  <div className="flex justify-between items-center">
+                    <div className="flex justify-between items-center w-full border-b">
+                      <h5 className="text-base lg:text-xl text-gray-600 my-4 w-3/5 max-w-[600px] ">
+                        1ML
+                      </h5>
+                      <p className="text-base lg:text-lg text-secondary font-medium w-2/5">
+                        £250
+                      </p>
+                    </div>
                   </div>
-                </div>
-                <div className="flex justify-between items-center">
-                  <div className="flex justify-between items-center w-full border-b">
-                    <h5 className="text-base lg:text-xl text-gray-600 my-4 w-3/5 max-w-[600px] ">
-                      2ML
-                    </h5>
-                    <p className="text-base lg:text-lg text-secondary font-medium w-2/5">
-                      £350
-                    </p>
+                  <div className="flex justify-between items-center">
+                    <div className="flex justify-between items-center w-full border-b">
+                      <h5 className="text-base lg:text-xl text-gray-600 my-4 w-3/5 max-w-[600px] ">
+                        2ML
+                      </h5>
+                      <p className="text-base lg:text-lg text-secondary font-medium w-2/5">
+                        £350
+                      </p>
+                    </div>
                   </div>
-                </div>
-                <div className="flex justify-between items-center">
-                  <div className="flex justify-between items-center w-full border-b">
-                    <h5 className="text-base lg:text-xl text-gray-600 my-4 w-3/5 max-w-[600px] ">
-                      3ML
-                    </h5>
-                    <p className="text-base lg:text-lg text-secondary font-medium w-2/5">
-                      £400
-                    </p>
+                  <div className="flex justify-between items-center">
+                    <div className="flex justify-between items-center w-full border-b">
+                      <h5 className="text-base lg:text-xl text-gray-600 my-4 w-3/5 max-w-[600px] ">
+                        3ML
+                      </h5>
+                      <p className="text-base lg:text-lg text-secondary font-medium w-2/5">
+                        £400
+                      </p>
+                    </div>
                   </div>
-                </div>
-                <div className="flex justify-between items-center">
-                  <div className="flex justify-between items-center w-full border-b">
-                    <h5 className="text-base lg:text-xl text-gray-600 my-4 w-3/5 max-w-[600px] ">
-                      4ML
-                    </h5>
-                    <p className="text-base lg:text-lg text-secondary font-medium w-2/5">
-                      £450
-                    </p>
+                  <div className="flex justify-between items-center">
+                    <div className="flex justify-between items-center w-full border-b">
+                      <h5 className="text-base lg:text-xl text-gray-600 my-4 w-3/5 max-w-[600px] ">
+                        4ML
+                      </h5>
+                      <p className="text-base lg:text-lg text-secondary font-medium w-2/5">
+                        £450
+                      </p>
+                    </div>
                   </div>
-                </div>
-              </details>
-              <details>
-                <summary>
-                  <h3 className="text-primary font-semibold text-xl mt-8 mb-2">
-                    Botox{" "}
-                  </h3>
-                </summary>
-                <div className="flex justify-between items-center">
-                  <div className="flex justify-between items-center w-full border-b">
-                    <h5 className="text-base lg:text-xl text-gray-600 my-4 w-3/5 max-w-[600px] ">
-                      1 AREA
-                    </h5>
-                    <p className="text-base lg:text-lg text-secondary font-medium w-2/5">
-                      £180
-                    </p>
+                </details>
+                <details>
+                  <summary>
+                    <h3 className="text-primary font-semibold text-xl mt-8 mb-2">
+                      Botox{" "}
+                    </h3>
+                  </summary>
+                  <div className="flex justify-between items-center">
+                    <div className="flex justify-between items-center w-full border-b">
+                      <h5 className="text-base lg:text-xl text-gray-600 my-4 w-3/5 max-w-[600px] ">
+                        1 AREA
+                      </h5>
+                      <p className="text-base lg:text-lg text-secondary font-medium w-2/5">
+                        £180
+                      </p>
+                    </div>
                   </div>
-                </div>
-                <div className="flex justify-between items-center">
-                  <div className="flex justify-between items-center w-full border-b">
-                    <h5 className="text-base lg:text-xl text-gray-600 my-4 w-3/5 max-w-[600px] ">
-                      2 AREAS
-                    </h5>
-                    <p className="text-base lg:text-lg text-secondary font-medium w-2/5">
-                      £240
-                    </p>
+                  <div className="flex justify-between items-center">
+                    <div className="flex justify-between items-center w-full border-b">
+                      <h5 className="text-base lg:text-xl text-gray-600 my-4 w-3/5 max-w-[600px] ">
+                        2 AREAS
+                      </h5>
+                      <p className="text-base lg:text-lg text-secondary font-medium w-2/5">
+                        £240
+                      </p>
+                    </div>
                   </div>
-                </div>
-                <div className="flex justify-between items-center">
-                  <div className="flex justify-between items-center w-full border-b">
-                    <h5 className="text-base lg:text-xl text-gray-600 my-4 w-3/5 max-w-[600px] ">
-                      3 AREAS
-                    </h5>
-                    <p className="text-base lg:text-lg text-secondary font-medium w-2/5">
-                      £300
-                    </p>
+                  <div className="flex justify-between items-center">
+                    <div className="flex justify-between items-center w-full border-b">
+                      <h5 className="text-base lg:text-xl text-gray-600 my-4 w-3/5 max-w-[600px] ">
+                        3 AREAS
+                      </h5>
+                      <p className="text-base lg:text-lg text-secondary font-medium w-2/5">
+                        £300
+                      </p>
+                    </div>
                   </div>
-                </div>
-                <div className="flex justify-between items-center">
-                  <div className="flex justify-between items-center w-full border-b">
-                    <h5 className="text-base lg:text-xl text-gray-600 my-4 w-3/5 max-w-[600px] ">
-                      4 AREAS
-                    </h5>
-                    <p className="text-base lg:text-lg text-secondary font-medium w-2/5">
-                      £400
-                    </p>
+                  <div className="flex justify-between items-center">
+                    <div className="flex justify-between items-center w-full border-b">
+                      <h5 className="text-base lg:text-xl text-gray-600 my-4 w-3/5 max-w-[600px] ">
+                        4 AREAS
+                      </h5>
+                      <p className="text-base lg:text-lg text-secondary font-medium w-2/5">
+                        £400
+                      </p>
+                    </div>
                   </div>
-                </div>
-              </details>
-              <details>
-                <summary>
-                  <h3 className="text-primary font-semibold text-xl mt-8 mb-2">
-                  Others
-                  </h3>
-                </summary>
-                <div className="flex justify-between items-center">
-                  <div className="flex justify-between items-center w-full border-b">
-                    <h5 className="text-base lg:text-xl text-gray-600 my-4 w-3/5 max-w-[600px] ">
-                    PROFHILO FIRST DOSE</h5>
-                    <p className="text-base lg:text-lg text-secondary font-medium w-2/5">
-                    £249
-                    </p>
+                </details>
+                <details>
+                  <summary>
+                    <h3 className="text-primary font-semibold text-xl mt-8 mb-2">
+                      Others
+                    </h3>
+                  </summary>
+                  <div className="flex justify-between items-center">
+                    <div className="flex justify-between items-center w-full border-b">
+                      <h5 className="text-base lg:text-xl text-gray-600 my-4 w-3/5 max-w-[600px] ">
+                        PROFHILO FIRST DOSE
+                      </h5>
+                      <p className="text-base lg:text-lg text-secondary font-medium w-2/5">
+                        £249
+                      </p>
+                    </div>
                   </div>
-                </div>
-                <div className="flex justify-between items-center">
-                  <div className="flex justify-between items-center w-full border-b">
-                    <h5 className="text-base lg:text-xl text-gray-600 my-4 w-3/5 max-w-[600px] ">
-                    SOSUM BOOSTER</h5>
-                    <p className="text-base lg:text-lg text-secondary font-medium w-2/5">
-                      £160
-                    </p>
+                  <div className="flex justify-between items-center">
+                    <div className="flex justify-between items-center w-full border-b">
+                      <h5 className="text-base lg:text-xl text-gray-600 my-4 w-3/5 max-w-[600px] ">
+                        SOSUM BOOSTER
+                      </h5>
+                      <p className="text-base lg:text-lg text-secondary font-medium w-2/5">
+                        £160
+                      </p>
+                    </div>
                   </div>
-                </div>
-              </details>
+                </details>
               </div>
             </div>
 
